fd808dd527c5fee4b4ad99427e68fe98ba308413
Mosi <mostafawshe@gmail.com>
2023-02-19 17:48:35 +0330

    Tabs Comp

---
+++ components/product/TabsComp.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="food_section layout_padding-bottom">
+        <div class="container">
+            <div class="heading_container heading_center">
+                <h2>
+                    منو محصولات
+                </h2>
+            </div>
+
+            <Tabs nav-class="filters_menu" nav-item-active-class="active" :options="{disableScrollBehavior:true}">
+                <Tab v-for="(tabList , index) in products.data.tabList" :key="index" :name="tabList">
+                    <div class="filters_content">
+                <div class="row grid">
+                    <div v-for="product in products.data.tabPanel[index]" :key="product.id" class="col-sm-6 col-lg-4">
+                        <ProductCardComp :product="product" />
+                    </div>
+                </div>
+            </div>
+                </Tab>
+            </Tabs>
+
+            <div class="btn-box">
+                <a href="">
+                    مشاهده بیشتر
+                </a>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+import {Tabs,Tab} from 'vue3-tabs-component'; 
+
+const {public: {apiBase}} = useRuntimeConfig();
+
+const {data: products , error} = await useFetch(`${apiBase}/products/products-tabs`);
+
+console.log(products.value)
+</script>
